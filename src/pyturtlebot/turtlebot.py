0f193b64088e390fca30a8a8ece652a21334e1b4
Jon Binney <jbinney@willowgarage.com>
2013-08-24 13:25:49 -0700

    add absolute rotation and translation functions

--- src/pyturtlebot/turtlebot.py
+++ src/pyturtlebot/turtlebot.py
@@ -1,10 +1,13 @@
 import sys
 import time
+import numpy as np
 
 import rospy
 
 from kobuki_msgs.msg import BumperEvent
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from tf import transformations as trans
 
 _turtlebot_singleton = None
 
@@ -24,10 +27,17 @@ class Turtlebot(object):
         rospy.init_node('pyturtlebot', anonymous=True)
         rospy.myargv(argv=sys.argv)
 
+        self.__x = None
+        self.__y = None
+        self.__angle = None
+        self.__cumulative_angle = 0.0
+        self.__have_odom = False
+
         self.on_bumper = None
 
         self.__cmd_vel_pub = rospy.Publisher('/mobile_base/commands/velocity', Twist)
         self.__bumper_sub = rospy.Subscriber('/mobile_base/events/bumper', BumperEvent, self.__bumper_handler)
+        self.__odom_sub = rospy.Subscriber('/odom', Odometry, self.__odom_handler)
 
     def move(self, linear=0.0, angular=0.0):
         # Bounds checking
@@ -47,6 +57,52 @@ class Turtlebot(object):
         self.say("Moving ('{linear}' m/s, '{angular}' rad/s)...".format(linear=linear, angular=angular))
         self.__cmd_vel_pub.publish(msg)
 
+    def move_distance(self, distance, velocity=1.0):
+        # No bounds checking because we trust people. Not like William.
+        r = rospy.Rate(1)
+        while not self.__have_odom and not rospy.is_shutdown():
+            self.say("Waiting for odometry")
+            r.sleep()
+
+        msg = Twist()
+        msg.linear.x = velocity
+        x0 = self.__x
+        y0 = self.__y
+        r = rospy.Rate(100)
+        while not rospy.is_shutdown():
+            d = ((self.__x - x0)**2 + (self.__y - y0)**2)**0.5
+            if d >= distance:
+                break
+
+            self.__cmd_vel_pub.publish(msg)
+            r.sleep()
+        msg.linear.x = 0.0
+        self.__cmd_vel_pub.publish(msg)
+
+    def turn_angle(self, angle, velocity=1.0):
+        # No bounds checking because we trust people. Not like William.
+        r = rospy.Rate(1)
+        while not self.__have_odom and not rospy.is_shutdown():
+            self.say("Waiting for odometry")
+            r.sleep()
+
+        msg = Twist()
+        if angle >= 0:
+            msg.angular.z = np.abs(velocity)
+        else:
+            msg.angular.z = -np.abs(velocity)
+        angle0 = self.__cumulative_angle
+        r = rospy.Rate(100)
+        while not rospy.is_shutdown():
+            a_diff = self.__cumulative_angle - angle0
+            if (angle > 0 and a_diff >= angle) or (angle < 0 and a_diff <= angle):
+                break
+
+            self.__cmd_vel_pub.publish(msg)
+            r.sleep()
+        msg.angular.z = 0.0
+        self.__cmd_vel_pub.publish(msg)
+
     def stop(self):
         msg = Twist()
         msg.linear.x = 0.0
@@ -61,6 +117,25 @@ class Turtlebot(object):
     def say(self, msg):
         print(msg)
         sys.stdout.flush()
+
+    def __odom_handler(self, msg):
+        self.__x = msg.pose.pose.position.x
+        self.__y = msg.pose.pose.position.y
+        q = msg.pose.pose.orientation
+        a = trans.euler_from_quaternion([q.x, q.y, q.z, q.w])[2]
+
+        # cumulative angle doesn't wrap. assumes we've not moved more than pi radians
+        # since last odom message
+        if self.__have_odom:
+            a_diff = a - self.__angle
+            if a_diff > np.pi:
+                a_diff -= 2*np.pi
+            elif a_diff < -np.pi:
+                a_diff += 2*np.pi
+            self.__cumulative_angle += a_diff
+
+        self.__angle = a
+        self.__have_odom = True
 
     def __bumper_handler(self, msg):
         if msg.state != BumperEvent.PRESSED or msg.bumper != BumperEvent.CENTER:
